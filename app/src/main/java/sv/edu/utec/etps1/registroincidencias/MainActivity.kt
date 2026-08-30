3369022315a5c5eac7ccffc16c97004238d80de8
esdanielvargas <hello@danielvargas.dev>
2026-08-30 17:17:50 -0600

    refactor: update ui for academic record and implement OutlinedTextField components

--- app/src/main/java/sv/edu/utec/etps1/registroincidencias/MainActivity.kt
+++ app/src/main/java/sv/edu/utec/etps1/registroincidencias/MainActivity.kt
@@ -2,7 +2,9 @@ package sv.edu.utec.etps1.registroincidencias
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,14 +12,22 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineBreak
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import sv.edu.utec.etps1.registroincidencias.ui.theme.RegistroIncidenciasTheme
@@ -25,6 +35,14 @@ import sv.edu.utec.etps1.registroincidencias.ui.theme.RegistroIncidenciasTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
+        )
+
         setContent {
             RegistroIncidenciasTheme {
                 RegistroIncidenciasApp()
@@ -32,6 +50,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @Composable
 fun RegistroIncidenciasApp() {
     Column(
@@ -42,12 +61,21 @@ fun RegistroIncidenciasApp() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Control de incidencias",
-            style = MaterialTheme.typography.headlineMedium
+            text = "Registro Académico",
+            style = MaterialTheme.typography.headlineMedium,
+            textAlign = TextAlign.Center
         )
         Spacer(modifier = Modifier.height(10.dp))
         Text(
-            text = "Completa los datos básicos del reporte."
+            text = "Ingresa los datos de la actividad para llevar el control y registro de cada evaluación del ciclo.",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium.merge(
+                TextStyle(
+                    lineBreak = LineBreak.Paragraph.copy(
+                        strategy = LineBreak.Strategy.Balanced
+                    )
+                )
+            )
         )
 
         Spacer(modifier = Modifier.height(24.dp))
@@ -55,33 +83,42 @@ fun RegistroIncidenciasApp() {
         OutlinedTextField(
             value = "",
             onValueChange = {},
-            label = { Text("Título") },
+            label = { Text("Título de la actividad") },
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(10.dp))
+        Spacer(modifier = Modifier.height(4.dp))
 
         OutlinedTextField(
             value = "",
             onValueChange = {},
-            label = { Text("Descripción") },
+            label = { Text("Calificación obtenida") },
             modifier = Modifier.fillMaxWidth()
         )
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(18.dp))
 
         Button(
             onClick = { },
-            modifier = Modifier.fillMaxWidth()
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(54.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0A0A0A))
         ) {
-            Text(text = "Crear reporte")
+            Text(text = "Guardar calificación")
         }
 
-        Spacer(modifier = Modifier.height(24.dp))
+        Spacer(modifier = Modifier.height(18.dp))
+        HorizontalDivider(thickness = 1.dp, color = Color.LightGray)
+        Spacer(modifier = Modifier.height(18.dp))
 
-        Card(modifier = Modifier.fillMaxWidth()) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFECECEC))
+        ) {
             Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = "Aún no hay una incidencia registrada.")
+                Text(text = "Aún no hay calificaciones registradas.")
             }
         }
     }
